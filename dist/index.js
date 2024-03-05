8c3fbd96103317e4cf49f40b2213e7f70a277d3f
Quentin Gérôme <quentin@letsbuildit.dev>
2024-03-05 14:41:50 +0100

    Fix tests & remove linter action

--- dist/index.js
+++ dist/index.js
@@ -7277,23 +7277,17 @@ async function run() {
         const workspace = core.getInput('workspace');
         const token = core.getInput('token');
         const url = core.getInput('url');
-        const install_python = core.getInput('install_python');
         const version = core.getInput('openhexa_version');
-        // Install python if requested
-        if (install_python === 'true') {
-            await exec.exec('sudo apt-get update');
-            await exec.exec('sudo apt-get install -y python3.11');
-        }
         // Install openhexa.sdk
         try {
             if (!version || semver_1.default.valid(version)) {
                 core.info(`Installing openhexa.sdk ${version}...`);
-                await exec.exec(`pip install openhexa.sdk${version ? `==${version}` : ''}`);
+                await exec.exec(`python -m pip install openhexa.sdk${version ? `==${version}` : ''}`);
             }
             else if (version) {
                 // We have a git branch
                 core.info(`Installing openhexa.sdk from branch ${version}`);
-                await exec.exec(`pip install https://github.com/blsq/openhexa-sdk-python/archive/refs/heads/${version}.zip`);
+                await exec.exec(`python -m pip install https://github.com/blsq/openhexa-sdk-python/archive/refs/heads/${version}.zip`);
             }
         }
         catch (err) {
@@ -7302,9 +7296,14 @@ async function run() {
         core.info('Installing openhexa.sdk... Done!');
         core.info('Configuring openhexa.sdk...');
         await exec.exec('openhexa', ['config', 'set_url', url]);
-        await exec.exec('openhexa', ['workspaces', 'add', workspace], {
+        core.info(`Adding workspace ${workspace} to openhexa.sdk...`);
+        const res = await exec.exec('openhexa', ['workspaces', 'add', workspace], {
             env: { HEXA_TOKEN: token }
         });
+        core.info(`${res}`);
+        if (res !== 0) {
+            throw new Error('Failed to add workspace to openhexa.sdk');
+        }
         core.info('Configuring openhexa.sdk... Done!');
         core.info("OpenHEXA is now configured and ready to use! Just run 'openhexa' to see the available commands.");
     }
--- src/main.ts
+++ src/main.ts
@@ -11,27 +11,20 @@ export async function run(): Promise<void> {
     const workspace: string = core.getInput('workspace')
     const token: string = core.getInput('token')
     const url: string = core.getInput('url')
-    const install_python = core.getInput('install_python')
     const version = core.getInput('openhexa_version')
-
-    // Install python if requested
-    if (install_python === 'true') {
-      await exec.exec('sudo apt-get update')
-      await exec.exec('sudo apt-get install -y python3.11')
-    }
 
     // Install openhexa.sdk
     try {
       if (!version || semver.valid(version)) {
         core.info(`Installing openhexa.sdk ${version}...`)
         await exec.exec(
-          `pip install openhexa.sdk${version ? `==${version}` : ''}`
+          `python -m pip install openhexa.sdk${version ? `==${version}` : ''}`
         )
       } else if (version) {
         // We have a git branch
         core.info(`Installing openhexa.sdk from branch ${version}`)
         await exec.exec(
-          `pip install https://github.com/blsq/openhexa-sdk-python/archive/refs/heads/${version}.zip`
+          `python -m pip install https://github.com/blsq/openhexa-sdk-python/archive/refs/heads/${version}.zip`
         )
       }
     } catch (err) {
@@ -42,9 +35,14 @@ export async function run(): Promise<void> {
     core.info('Installing openhexa.sdk... Done!')
     core.info('Configuring openhexa.sdk...')
     await exec.exec('openhexa', ['config', 'set_url', url])
-    await exec.exec('openhexa', ['workspaces', 'add', workspace], {
+    core.info(`Adding workspace ${workspace} to openhexa.sdk...`)
+    const res = await exec.exec('openhexa', ['workspaces', 'add', workspace], {
       env: { HEXA_TOKEN: token }
     })
+    core.info(`${res}`)
+    if (res !== 0) {
+      throw new Error('Failed to add workspace to openhexa.sdk')
+    }
     core.info('Configuring openhexa.sdk... Done!')
     core.info(
       "OpenHEXA is now configured and ready to use! Just run 'openhexa' to see the available commands."
